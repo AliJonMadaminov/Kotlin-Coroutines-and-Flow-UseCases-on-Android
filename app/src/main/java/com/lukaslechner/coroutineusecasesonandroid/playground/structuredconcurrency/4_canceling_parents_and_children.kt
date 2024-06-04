d9c17eb6dfea3c3701b05f1537bc35a165f8f9ae
Ali <alimadaminov06@gmail.com>
2024-06-04 15:44:51 +0500

    canceling parent and children

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/structuredconcurrency/4_canceling_parents_and_children.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/structuredconcurrency/4_canceling_parents_and_children.kt
@@ -1,5 +1,67 @@
 package com.lukaslechner.coroutineusecasesonandroid.playground.structuredconcurrency
 
-fun main() {
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlin.coroutines.cancellation.CancellationException
 
+fun main() = runBlocking {
+    cancelingChildDoesNotCancelParentAndSiblings()
+}
+
+private suspend fun cancelingParentCancelsChildren() {
+    val scope = CoroutineScope(Dispatchers.Default)
+    scope.launch {
+        delay(1000)
+        println("Coroutine 1 has completed")
+    }.invokeOnCompletion { throwable ->
+        if (throwable is CancellationException) {
+            println("Coroutine 1 cancelled")
+        }
+    }
+    scope.launch {
+        delay(1000)
+        println("Coroutine 2 has completed")
+    }.invokeOnCompletion { throwable ->
+        if (throwable is CancellationException) {
+            println("Coroutine 2 cancelled")
+        }
+    }
+
+    delay(500)
+    scope.cancel()
+    println("Scope job cancelled")
+}
+
+private suspend fun cancelingChildDoesNotCancelParentAndSiblings() {
+    val scope = CoroutineScope(Dispatchers.Default)
+    val childJob = scope.launch {
+        delay(1000)
+        println("Coroutine 1 has completed")
+    }
+    childJob.invokeOnCompletion { throwable ->
+        if (throwable is CancellationException) {
+            println("Coroutine 1 cancelled")
+        }
+    }
+    scope.launch {
+        delay(1000)
+        println("Coroutine 2 has completed")
+    }.invokeOnCompletion { throwable ->
+        if (throwable is CancellationException) {
+            println("Coroutine 2 cancelled")
+        }
+    }
+    scope.coroutineContext.job.invokeOnCompletion { throwable ->
+        if (throwable is CancellationException) {
+            println("Parent job cancelled")
+        }
+    }
+    childJob.cancelAndJoin()
+    delay(1000)
 }
