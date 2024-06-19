97f8f6172e8f626e2ca5669d185cfd649639de30
Ali <alimadaminov06@gmail.com>
2024-06-19 18:27:42 +0500

    handling coroutine exception with try-catch

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase13/ExceptionHandlingViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase13/ExceptionHandlingViewModel.kt
@@ -1,16 +1,24 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase13
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
 import kotlinx.coroutines.*
 import timber.log.Timber
 
-class ExceptionHandlingViewModel(
+class  ExceptionHandlingViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun handleExceptionWithTryCatch() {
-
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
+            try {
+                val result = api.getAndroidVersionFeatures(27)
+            } catch (e: Exception) {
+                uiState.value = UiState.Error("Network request failed: $e")
+            }
+        }
     }
 
     fun handleWithCoroutineExceptionHandler() {
