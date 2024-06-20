823f3fecc6cad5ba2efae04ca96aafb2123ca0c3
Ali <alimadaminov06@gmail.com>
2024-06-20 18:01:44 +0500

    runTest{} and virtual time

--- app/src/test/java/com/lukaslechner/coroutineusecasesonandroid/playground/1_virtual_time.kt
+++ app/src/test/java/com/lukaslechner/coroutineusecasesonandroid/playground/1_virtual_time.kt
@@ -1,8 +1,13 @@
 package com.lukaslechner.coroutineusecasesonandroid.playground
 
 import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.currentTime
+import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
 class SystemUnderTest {
@@ -14,10 +19,17 @@ class SystemUnderTest {
 
 class TestClass {
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun `functionWithDelay() should return 42`() {
+    fun `functionWithDelay() should return 42`() = runTest {
+        val realTimeStart = System.currentTimeMillis()
+        val virtualTimeStart = currentTime
         val systemUnderTest = SystemUnderTest()
-        val result = runBlocking { systemUnderTest.functionWithDelay() }
+        val result = systemUnderTest.functionWithDelay()
         assertEquals(42, result)
+        val realTimeDuration = System.currentTimeMillis() - realTimeStart
+        val virtualTimeDuration = currentTime - virtualTimeStart
+        println("Real time duration: $realTimeDuration")
+        println("Virtual time duration: $virtualTimeDuration")
     }
 }
