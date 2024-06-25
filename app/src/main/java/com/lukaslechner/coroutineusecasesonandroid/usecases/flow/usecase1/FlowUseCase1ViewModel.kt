220d2e70a9db8ceee796fa5031b91b9fae0f0e3c
Ali <alimadaminov06@gmail.com>
2024-06-25 17:51:10 +0500

    showing flow data in ui

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
@@ -1,12 +1,26 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.flow.usecase1
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class FlowUseCase1ViewModel(
     stockPriceDataSource: StockPriceDataSource
 ) : BaseViewModel<UiState>() {
 
-    val currentStockPriceAsLiveData: LiveData<UiState> = TODO()
+    private val _currentStockPriceAsLiveData = MutableLiveData<UiState>()
+    val currentStockPriceAsLiveData: LiveData<UiState> = _currentStockPriceAsLiveData
+
+    init {
+        viewModelScope.launch {
+            stockPriceDataSource.latestStockList.collect { stockList ->
+                _currentStockPriceAsLiveData.value = UiState.Success(stockList)
+            }
+        }
+    }
 
 }
