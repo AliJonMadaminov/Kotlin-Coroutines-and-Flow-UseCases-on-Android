c7a1dd1c7681e823f7738c09e988f054152285cb
Ali <alimadaminov06@gmail.com>
2024-06-30 12:53:56 +0500

    filter intermediate operator

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/flow/intermediate_operators/2_filter.kt
@@ -0,0 +1,28 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.flow.intermediate_operators
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.filterIsInstance
+import kotlinx.coroutines.flow.filterNot
+import kotlinx.coroutines.flow.filterNotNull
+
+suspend fun main() {
+    listOf(1, 2, null, 3, 3.5f, 4, 5).asFlow()
+        .filterNotNull()
+        .filterIsInstance<Int>()
+        .printEvenNumbers()
+        .printOddNumbers()
+}
+
+private suspend fun Flow<Int>.printEvenNumbers(): Flow<Int> {
+    filter { it % 2 == 0 }.collect { print("$it ") }
+    println()
+    return this
+}
+
+private suspend fun Flow<Int>.printOddNumbers(): Flow<Int> {
+    filterNot { it % 2 == 0 }.collect { print("$it ") }
+    println()
+    return this
+}
